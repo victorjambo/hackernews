982c8ea50c55d50c3c1594d5d8764b78fd06945c
victorjambo <victorjambo@live.com>
2017-03-18 07:18:28 +0300

    auth

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,30 +1,23 @@
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
-  # GET /submissions
-  # GET /submissions.json
   def index
     @submissions = Submission.all
   end
 
-  # GET /submissions/1
-  # GET /submissions/1.json
   def show
   end
 
-  # GET /submissions/new
   def new
-    @submission = Submission.new
+    @submission = current_user.submissions.build
   end
 
-  # GET /submissions/1/edit
   def edit
   end
 
-  # POST /submissions
-  # POST /submissions.json
   def create
-    @submission = Submission.new(submission_params)
+    @submission = current_user.submissions.build(submission_params)
 
     respond_to do |format|
       if @submission.save
@@ -37,8 +30,6 @@ class SubmissionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /submissions/1
-  # PATCH/PUT /submissions/1.json
   def update
     respond_to do |format|
       if @submission.update(submission_params)
@@ -51,8 +42,6 @@ class SubmissionsController < ApplicationController
     end
   end
 
-  # DELETE /submissions/1
-  # DELETE /submissions/1.json
   def destroy
     @submission.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class SubmissionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_submission
       @submission = Submission.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
       params.require(:submission).permit(:title, :url)
     end
